b31238424f75d9e61549f92015a85c15e95e85f9
Migg88 <miguel141295@hotmail.com>
2019-10-18 18:29:06 -0500

    query hotels page data

--- src/templates/hotel-page.js
+++ src/templates/hotel-page.js
@@ -5,15 +5,56 @@ import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import "../components/main.scss"
 
-export const HotelPageTemplate = ({}) => (
-	<h1>Works!</h1>
+export const HotelPageTemplate = () => (
+	<div>
+		<div 
+			className="h-100 w-100"
+		>
+		</div>
+	</div>
 )
 
-const HotelPage = ({}) => {
+const HotelPage = () => {
 	return (
-		<HotelPageTemplate />
+		<Layout>
+			<HotelPageTemplate />
+		</Layout>
 	)
 }
 
 export default HotelPage
+
+export const HotelPageQuery = graphql`
+	query HotelPage($id: String!) {
+		markdownRemark(id: {eq: $id }) {
+			frontmatter {
+				title
+				image {
+					childImageSharp {
+						fluid(maxWidth: 1920, quality: 100) {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
+				brand {
+					childImageSharp {
+						fluid(maxWidth:600) {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
+				description
+				destination_img {
+					childImageSharp {
+						fluid(maxWidth: 1024) {
+							...GatsbyImageSharpFluid
+						}
+					}
+				}
+				destination_name
+				destination_description
+			}
+		}
+	}
+`
 
